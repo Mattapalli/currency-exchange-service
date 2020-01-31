144f07f903e87f94d6c6b719f10dffbc05ed9ba3
RMattapalli <RMattapalli@HIBAML134346.ad.harman.com>
2020-01-31 12:24:09 +0530

    adding spring cloud sleuth

--- currency-exchange-service/src/main/java/com/rk/currencyexchange/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/rk/currencyexchange/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,9 @@ package com.rk.currencyexchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,5 +14,11 @@ public class CurrencyExchangeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
 
 }
--- currency-exchange-service/src/main/java/com/rk/currencyexchange/rest/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/rk/currencyexchange/rest/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.rk.currencyexchange.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import com.rk.currencyexchange.repository.ExchangeValueRepository;
 
 @RestController
 public class CurrencyExchangeController {
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
     @Autowired
 	Environment env;
 	@Autowired
@@ -20,6 +23,7 @@ public class CurrencyExchangeController {
 		
 		
 		ExchangeValue exchange= repository.findByFromAndTo(from, to);
+		logger.info("{}",exchange);
 		System.out.println("Exchange:="+exchange);
 		 exchange.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		 return exchange;
